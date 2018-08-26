ea01bd89d8e8d81dfe2ecfface6e59f0db36e100
superposition <superposition@SM41203152272.fios-router.home>
2018-08-25 20:03:30 -0400

    --wip-- [skip ci]

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs');
 
+
 var app = http.createServer(function (req, res) {
   if (req.url.indexOf('/img') != -1) {
     var filePath = req.url.split('/img')[1];
