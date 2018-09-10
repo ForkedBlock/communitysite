13061aa36517930cca4098a9939b778e8e5828ba
superposition <ericmanganaro@gmail.com>
2018-09-10 15:37:41 -0400

    breakingchange
    sorryabout that

--- public/js/index.js
+++ public/js/index.js
@@ -48,8 +48,7 @@ var App = React.createClass({ displayName: 'App',
 			//	React.createElement(TitleList, { title: 'Trending now', url: 'discover/movie?sort_by=popularity.desc&page=1' }),
 			//	React.createElement(TitleList, { title: 'Most watched in Horror', url: 'genre/27/movies?sort_by=popularity.desc&page=1' }),
 			//	React.createElement(TitleList, { title: 'Sci-Fi greats', url: 'genre/878/movies?sort_by=popularity.desc&page=1' }),
-			//	React.createElement(TitleList, { title: 'Comedy magic', url: 'genre/35/movies?sort_by=popularity.desc&page=1' })));
-///great
+				React.createElement(TitleList, { title: 'Comedy magic', url: 'genre/35/movies?sort_by=popularity.desc&page=1' })));
 
 	} });
 
